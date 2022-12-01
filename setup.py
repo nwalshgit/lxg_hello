f5a973c52f2f070a80ce8424e1ab865726ff1173
Nathan Walsh <nwalsh@github.com>
2022-11-30 23:08:22 -0500

    rename pyproject.toml

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ setup(
     description='A small example package',
     url='https://github.com/nwalshgit/lxg_hello',
     author='Nathan Walsh <nwalsh@example.com>',
-    license='MIT',
-    packages=['lxg_hello'],
+    #packages=['lxg_hello'],
     install_requires=[
        "pandas>=1.5.0",
-       "lxg-world==0.0.1",
+       "lxg_world @ git+https://github.com/nwalshgit/lxg_world@master",
     ],
     dependency_links=[
         #"git+ssh://git@github.com/nwalshgit/lxg_world.git@0.0.1#egg=lxg_world",
-        "https://github.com/nwalshgit/lxg_world.git#egg=lxg_world-0.0.1",
+        #"https://github.com/nwalshgit/lxg_world.git#egg=lxg_world",
         #"https://github.com/dmfigol/smartsheet-python.sdk/archive/no-setuptools-scm.zip#egg=smartsheet-python-sdk-10.1.3.3"
     ],
     zip_safe=False
